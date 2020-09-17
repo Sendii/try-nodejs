8880ba5eff2f41fc4b21e5edaba21c55e3bd3a3d
Sendii <sendidianhadiwijaya@gmail.com>
2020-09-17 13:13:24 +0700

    Update alert

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
+const session = require('express-session')
 const app = express()
 
 const conn = mysql.createConnection({
@@ -24,22 +25,41 @@ conn.connect((err) => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.use(bodyParser.json())
+app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
 app.use('/assets', express.static(__dirname + '/public'))
 
-app.listen(8000, function(){
+var sessMessage = ''
+var sessActive = false
+function textSession(message){
+	return message
+}
+
+app.listen(3000, function(){
 	// list data product
 	app.get('/', (req, res) => {
-		let sql = "SELECT * FROM PRODUCTs";
+		let sql = "SELECT * FROM PRODUCT ORDER BY product_id DESC";
 		let query = conn.query(sql, (err, result) => {
 			if (err){
 				console.log(err.message)
 			}
+			sessMessage = req.session
+
+			if (sessActive){					
+				sessMessage.msg = "Berhasil menambahkan data"
+				console.log('1:' + sessActive)
+			}
+
 			res.render('product_view', {
-				results: result
+				results: result,
+				session: textSession(sessMessage),
+				sessCheck: sessActive
 			})
+			sessActive = false
+			console.log('2:' + sessActive)
+			req.session.destroy()
 		})
 	})
 
@@ -53,9 +73,10 @@ app.listen(8000, function(){
 		let query = conn.query(sql, data, (err, result) => {
 			if (err) {
 				console.log(err.message)
-			}else{
+			}else{				
+				sessActive = true
 				console.log("Berhasil menambahkan data")
-			}
+			}						
 			res.redirect('/')
 		})
 	})
